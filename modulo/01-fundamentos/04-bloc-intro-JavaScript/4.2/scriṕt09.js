94e2afb76a49a6b4407712add6bbc565fbd6cfd5
Lira Lucas <lucas.lira270493@gmail.com>
2021-07-31 23:44:20 -0300

    adiciona exe-9 dia 4.2

---
+++ modulo/01-fundamentos/04-bloc-intro-JavaScript/4.2/scriṕt09.js
@@ -0,0 +1,9 @@
+//Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2 .
+
+let num = []; 
+
+
+for (num = 1; num <= 25; num += 1) { 
+    console.log(num)
+}
+//...
