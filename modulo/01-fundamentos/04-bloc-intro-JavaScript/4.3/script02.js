376406e2064887c86508ad6b1e09dab784e17e03
Lira Lucas <lucas.lira270493@gmail.com>
2021-08-02 15:05:32 -0300

    tenta corrigir cofigo

--- modulo/01-fundamentos/04-bloc-intro-JavaScript/4.3/script02.js
+++ modulo/01-fundamentos/04-bloc-intro-JavaScript/4.3/script02.js
@@ -4,3 +4,9 @@ mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:*
 //Definir o numero de linhas e colunas 
 
 
+let asteriscos = '';
+
+for (let linha = 1; linha <= 5; linha += 1) {
+    asteriscos += '*';
+    console.log(asteriscos);
+}
