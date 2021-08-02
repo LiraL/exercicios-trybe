ac543bfd6deca8eddf628d2e6ec5ecc2540fd4af
Lira Lucas <lucas.lira270493@gmail.com>
2021-08-02 16:17:55 -0300

    adiciona test dia.

--- modulo/01-fundamentos/04-bloc-intro-JavaScript/4.4/test.js
+++ modulo/01-fundamentos/04-bloc-intro-JavaScript/4.4/test.js
@@ -1,4 +1,4 @@
-let name = 'Marta';
+/*let name = 'Marta';
 let lastName = 'Silva';
 let age = 34;
 let medals = { golden: 2, silver: 3 };
@@ -15,4 +15,39 @@ identity ['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018];
 
 console.log('A jogador ' + identity['name'] + ' ' + identity['lastName'] + ' foi eleita a melhot do mundo por 6 vezes ' + identity['bestInTheWorld']);
 
-console.log('A jogadora também possui ' + medals['golden'] + ' de ouro ' + medals['silver'] + ' de prata.'); 0
+console.log('A jogadora também possui ' + medals['golden'] + ' de ouro ' + medals['silver'] + ' de prata.'); 
+*//*
+
+let names = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge' 
+  };
+
+let person = {
+    person1: 'João',
+    person2: 'Maria',
+    person3: 'Jorge',
+};
+for (let saudar in person) {
+    console.log('Olá, ' + person[saudar] + '!')
+};
+*//*
+
+let car = {
+    model: 'A3 Sedan',
+    manufacturer: 'Audi',
+    year: 2020
+  };
+  for (let imprime in car) {
+      console.log(imprime, car[imprime])
+  };
+  */
+
+
+var a = 2;
+var b = 3;
+
+function soma (a, b) {
+    return a+ b;
+}
