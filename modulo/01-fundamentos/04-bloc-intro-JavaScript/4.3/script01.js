ab5f8c95d403e6810f45d95d691bc9db8818efb7
Lira Lucas <lucas.lira270493@gmail.com>
2021-08-02 10:14:22 -0300

    adiciona exe-2 dia 4.3

---
+++ modulo/01-fundamentos/04-bloc-intro-JavaScript/4.3/script01.js
@@ -0,0 +1,10 @@
+/*Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, 
+seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
+Por exemplo:*/
+
+let asteriscos = '';
+
+for (let linha = 1; linha <= 5; linha += 1) {
+    asteriscos += '*';
+    console.log(asteriscos)
+}
--- modulo/01-fundamentos/04-bloc-intro-JavaScript/4.3/script02.js
+++ modulo/01-fundamentos/04-bloc-intro-JavaScript/4.3/script02.js
@@ -1,12 +1,6 @@
-/*Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, 
-seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . 
-Por exemplo:*/
+/*Para o segundo exercício, faça o mesmo que antes, 
+mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:*/
 
 //Definir o numero de linhas e colunas 
 
-let asteriscos = '';
 
-for (let linha = 1; linha <= 5; linha += 1) {
-    asteriscos += '*';
-    console.log(asteriscos)
-}
